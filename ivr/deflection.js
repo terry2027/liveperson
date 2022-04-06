bab0c04c7ed5dec0271bb2d40c121d02332834f7
Terence Thambi Rajah <terence_thambi@yahoo.com>
2022-04-06 21:09:33 +1000

    Using phone number as from param

--- ivr/deflection.js
+++ ivr/deflection.js
@@ -1,7 +1,8 @@
 exports.handler = function(context, event, callback) {
     // REPLACE THESE
     let bitly_link = "https://bit.ly/3LC1uf4";
-    let sms_sender_name = "LivePerson"
+    //let sms_sender_name = "LivePerson"
+    let sms_sender_name = "+61481614007"
         // END
 
     let twiml = new Twilio.twiml.VoiceResponse()
@@ -10,7 +11,7 @@ exports.handler = function(context, event, callback) {
     context.getTwilioClient().messages.create({
         to: event.From,
         from: sms_sender_name,
-        body: "Start messaging with us here: \n " + bitly_link
+        body: "Thank you for selecting to message with us. Just reply to this SMS or start messaging with us here: \n " + bitly_link
     }).then(msg => {
         twiml.hangup();
         callback(null, twiml);
